fa92e3072de0846eff8bd2ec9d67a3bef763591b
Piyushzingade <piyushzingade@gmail.com>
2024-12-16 18:54:37 +0530

    Added backend URL

--- src/operations/api.tsx
+++ src/operations/api.tsx
@@ -1,7 +1,10 @@
-export const BASE_URL="http://localhost:3000"
+// export const BASE_URL =
+//   "https://second-brain-dcnd.vercel.app/?vercelToolbarCode=mwYNP7PN3YeashF";
 
 
-// export const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const BASE_URL =
+  "https://second-brain-dcnd.vercel.app/?vercelToolbarCode=mwYNP7PN3YeashF";
 export const endPoints={
     SIGN_UP:`${BASE_URL}/api/v1/signup`,
     SIGN_IN:`${BASE_URL}/api/v1/signin`,
